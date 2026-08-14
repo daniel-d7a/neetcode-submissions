3e0d88669daf8c977cbdecb1b84329a8a78109f2
Eyad Alsherif <71134399+daniel-d7a@users.noreply.github.com>
2026-08-14 03:51:38 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,33 @@
+func isPalindrome(s string) bool {
+
+	left := 0
+	right := len(s) - 1
+
+	for left < right {
+		// for left < len(s) && !isAlphanumeric(rune(s[left])) {
+		for left < right && !isAlphanumeric(rune(s[left])) {
+			left = left + 1
+		}
+		// for right >= 0 && !isAlphanumeric(rune(s[right])) {
+		for right > left && !isAlphanumeric(rune(s[right])) {
+			right = right - 1
+		}
+		// if right < 0 || left >= len(s) {
+		// 	break
+		// }
+
+		leftStr := strings.ToLower(string(s[left]))
+		rightStr := strings.ToLower(string(s[right]))
+
+		if leftStr != rightStr {
+			return false
+		}
+		left = left + 1
+		right = right - 1
+	}
+	return true
+}
+
+func isAlphanumeric(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsDigit(r)
+}
