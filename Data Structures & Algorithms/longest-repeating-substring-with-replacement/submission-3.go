fef86ae897a04346cf127471a4293cab862c3991
Eyad Alsherif <71134399+daniel-d7a@users.noreply.github.com>
2026-08-21 18:50:03 +0300

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.go
@@ -0,0 +1,29 @@
+func characterReplacement(s string, k int) int {
+
+	charMap := make(map[string]int)
+
+	l := 0
+
+	answer := 0
+
+	for r := range s {
+
+		charMap[string(s[r])]++
+		for r - l + 1 - getMaxFreq(charMap) > k{
+			charMap[string(s[l])]--
+			l++
+		}
+
+		answer = max(answer, r - l + 1)
+	}
+
+	return answer
+}
+
+func getMaxFreq(charMap map[string]int) int {
+	maxFreq := 0
+	for _, val := range charMap {
+		maxFreq = max(maxFreq, val)
+	}
+	return maxFreq
+}
