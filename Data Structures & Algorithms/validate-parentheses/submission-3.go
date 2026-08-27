3d69fe1fc2fb7c17a7b7d4516f286138db976575
Eyad Alsherif <71134399+daniel-d7a@users.noreply.github.com>
2026-08-27 04:01:03 +0300

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.go
@@ -0,0 +1,23 @@
+func isValid(s string) bool {
+	stack := []string{}
+	for _, c := range s {
+		
+		x := string(c)
+
+		if x == "(" || x == "{" || x == "[" {
+			stack = append(stack, x)
+		} else if len(stack) > 0 {
+			top := stack[len(stack) - 1]
+
+			if (top == "{" && x == "}") || (top == "(" && x == ")") || (top == "[" && x == "]") {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
