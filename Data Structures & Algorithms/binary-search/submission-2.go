a4e4c37521b7be54804e1154e1772b5445b8f407
Eyad Alsherif <71134399+daniel-d7a@users.noreply.github.com>
2026-09-03 05:23:08 +0300

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,20 @@
+func search(nums []int, target int) int {
+
+	mid := len(nums) / 2
+	l := 0
+	r := len(nums) - 1
+
+	for l <= r {
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+
+		mid = (r + l) / 2
+	} 
+
+	return -1
+}
